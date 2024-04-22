a75af9f4f03f736bf8ad128254de80fc4d6ae524
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-04-22 22:06:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 //your JS code here. If required.
 function submitForm(event) {
-    event.preventDefault(); // Prevent form submission
+    event.preventDefault();
     var firstName = document.getElementById('firstName').value;
     var lastName = document.getElementById('lastName').value;
     var phoneNumber = document.getElementById('phoneNumber').value;
     var email = document.getElementById('email').value;
 
-    // Construct the alert message with spaces
     var alertMessage = "First Name: " + firstName + " " +
                        "Last Name: " + lastName + " " +
                        "Phone Number: " + phoneNumber + " " +
@@ -14,3 +13,4 @@ function submitForm(event) {
 
     // Display the alert
     alert(alertMessage);
+}
